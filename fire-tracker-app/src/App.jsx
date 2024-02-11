1601795e4cfbc774aed67b58cbb23fde13f0d18c
horacenjoroge <125595726+horacenjoroge@users.noreply.github.com>
2024-02-11 05:21:21 +0300

    added fetch API

--- fire-tracker-app/src/App.jsx
+++ fire-tracker-app/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
-
-
+import { useState,useEffect } from 'react'
 import './App.css'
 import Map from './Components/Map'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [eventData, setEventData] = useState([])
+  const[loading, setLoading] = useState(false)
+
+  useEffect(() =>{
+    const fetchEvents =async() =>{
+      setLoading(true)
+      const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events')
+      const {events}=await res.json()
+      setEventData(events)
+      setLoading(false)
+
+
+    }
+    fetchEvents()
+  },[])
+  
 
   return (
     <>
